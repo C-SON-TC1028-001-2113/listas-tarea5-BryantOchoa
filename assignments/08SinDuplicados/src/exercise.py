61f8995b70d566670903718c5984ec084c1bb4af
BryantOchoa <A01252419@itesm.mx>
2021-09-28 06:12:47 +0000

    tarea bryant

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,7 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    x = int(input())
+    i = 0
+    lista = []
 
-
+    if x>0:
+        while i<x :
+            valor = input()
+            lista.append(valor)
+            i = i+1
+        print(lista)
+        sindups = []
+        for i in lista :
+            if i not in sindups:
+                sindups.append(i)
+        print(sindups)
+    else:
+        print('Error')
     pass
 
 if __name__=='__main__':
--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,15 @@
-
-
 def main():
-
-
-
+    x = int(input(''))
+    y = int(input(''))
+    z = x*y
+    lista = []
+    i=1
+    while i<=z:
+        i=i+1
+        w = int(input(''))
+        if w<10:
+            lista.append(w)
+    print(lista)
 
 if __name__=='__main__':
     main()
